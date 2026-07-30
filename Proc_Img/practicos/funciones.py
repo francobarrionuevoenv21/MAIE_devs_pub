1054961e633553143fd5de0f433b63011d4dacb2
Franco David Barrionuevo <78272951+francobarrionuevoenv21@users.noreply.github.com>
2026-07-30 16:00:51 -0300

    update jul 30 maie dev

--- Proc_Img/practicos/funciones.py
+++ Proc_Img/practicos/funciones.py
@@ -6,6 +6,8 @@
 
 # Importo liberías de base
 import numpy as np
+import rasterio
+from rasterio.windows import Window
 
 # Funciones creadas
 def scale(imagen, p, nodata = None):
@@ -24,3 +26,28 @@ def scale(imagen, p, nodata = None):
 
   # Retornar la imagen ecualizada
   return ecualizada
+
+def subset_img(ds, bands_list, ul_x, ul_y, lr_x, lr_y):
+    '''
+    Parámetros:
+    ds: Rasterio dataset;
+    bands_list: Lista de bandas a seleccionar. Si es singleband: [1];
+    ul_x: Coordenada x del extremo superior izquierdo;
+    ul_y: Coordenada y del extremo superior izquierdo;
+    lr_x: Coordenada x del extremo inferior derecho;
+    lr_y: Coordenada y del extremo inferior derecho;
+    ---
+    Return
+    subset: dataarray recortado según las coordenadas indicadas; 
+    window: Elemento Window de rasterio según las coordenadas indicadas
+    '''
+    row_off, col_off = ds.index(ul_x, ul_y) # https://rasterio-readthedocs-io.translate.goog/en/stable/quickstart.html?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=tc#spatial-indexing
+    ps_x = ds.transform.a
+    ps_y = ds.transform.e*(-1)
+    width = abs(round((lr_x-ul_x)/ps_x))
+    height = abs(round((ul_y-lr_y)/ps_y))
+
+    window = Window(col_off, row_off, width, height) # (col_off, row_off, width, height)
+    subset = ds.read(bands_list, window = window)[0] # [0] --> convierto de 3D a 2D
+
+    return subset, window
